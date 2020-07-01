97d8b751a1e30345f1cf76f5d7a5e5ac365e4736
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-01 09:45:04 -0400

    have json for employee's and working on components

---
+++ directory/src/components/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+const List = (props) => {
+    return(
+        <Fragment>
+            <div className="columns is-centered">
+                <div className="column is-one-quarter">
+
+                </div>
+                <div className="column is-one-quarter">
+
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default List
